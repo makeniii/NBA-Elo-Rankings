4ec8697ca9e0056bb5d4a269729609c25508175b
makeniii <makenifililava@gmail.com>
2022-11-11 23:58:46 +1100

    updates

--- nba-app.py
+++ nba-app.py
@@ -30,49 +30,50 @@ def create_team_names() -> List[str]:
 class Game():
     def __init__(self, teams: pd.DataFrame):
                   # this could be changed to a dictionary with home and away keys
-        self.teams = dict()
-        self.weight = 20
-
-        if 'vs.' in teams.MATCHUP[0]:
-            self.teams['HOME'] = teams.iloc[[0]]
-            self.teams['AWAY'] = teams.iloc[[1]]
-        else:
-            self.teams['AWAY'] = teams.iloc[[0]]
-            self.teams['HOME'] = teams.iloc[[1]]
+        self.teams = teams
 
     def __str__(self) -> str:
         return 'id: ' + str(self.id) + ', home_team: ' + self.home_team + ', away_team: ' + self.away_team + ', score: ' + str(self.score) + ', ' + self.date
     
     # do I need both?
     def __repr__(self) -> str:
-        return 'Home: ' + str(self.teams['HOME']) + '\nAway: ' + str(self.teams['AWAY']) + '\nWeight: ' + str(self.weight)
+        return str(self.teams) + '\nWeight: ' + str(self.weight) + '\n'
 
+class RegularGame(Game):
+    weight = 20
+
+    def __init__(self, teams: pd.DataFrame):
+        super().__init__(teams)
 
 class PlayInGame(Game):
+    weight = 24
+
     def __init__(self, teams: pd.DataFrame):
         super().__init__(teams)
-        self.weight = 24
 
 class FirstRoundGame(Game):
+    weight = 30
+
     def __init__(self, teams: pd.DataFrame):
         super().__init__(teams)
-        self.weight = 30
 
 class SecondRoundGame(Game):
-    def __init__(self, teams: pd.DataFrame):
-        super().__init__(teams)
-        self.weight = 40
-        
+    weight = 40
 
-class ConferenceFinalGame(Game):
     def __init__(self, teams: pd.DataFrame):
         super().__init__(teams)
-        self.weight = 50
+        
+class ConferenceFinalGame(Game):
+    weight = 50
+
+    def __init__(self, teams: pd.DataFrame):
+        super().__init__(teams)
 
 class FinalGame(Game):
+    weight = 60
+
     def __init__(self, teams: pd.DataFrame):
         super().__init__(teams)
-        self.weight = 60
 
 '''
 Deciding to keep the original type(pd.DataFrame) returned from the nba api because the methods that come with
@@ -80,17 +81,62 @@ are great and I might even use some more later down the line. Of course, I still
 '''
 class Schedule():
     def __init__(self) -> None:
-        self.games = list() 
+        self.games = pd.DataFrame() 
 
     def __str__(self) -> str:
         return str(self.games)
 
     def add_games(self, games) -> None:
-        self.games.append(games)
+        self.games = pd.concat([self.games, games]).reset_index(drop=True)
     
 class SeasonSchedule(Schedule): # Created this class simply for readability
     def __init__(self) -> None:
         super().__init__()
+
+    def initialise(self, year):
+        regular_season_games_curr = get_games(year, 'Regular Season')
+        regular_season_games_curr.drop(columns=['VIDEO_AVAILABLE'], inplace=True)
+        regular_season_games_curr = regular_season_games_curr.sort_values(by=['GAME_ID']).reset_index(drop=True)
+        regular_season_games_curr['LOCATION'] = np.where(regular_season_games_curr['MATCHUP'].str.contains('vs.'), 'Home', 'Away')
+        regular_season_games_curr['GAME_TYPE'] = 'Regular Season'
+        regular_season_games_curr['ID'] = pd.factorize(regular_season_games_curr['GAME_ID'])[0] + 1
+        regular_season_games_curr = regular_season_games_curr.set_index('ID')
+
+        # print(regular_season_games_curr)
+
+        # Duplicate game_ids are the stat lines of the two teams, so combine into one game
+        for i in range(0, len(regular_season_games_curr.index), 2):
+            self.add_games(pd.concat([regular_season_games_curr.iloc[[i]], regular_season_games_curr.iloc[[i+1]]]).reset_index(drop=True))
+
+        playoff_games_curr = get_games(year, 'Playoffs')
+        playoff_games_curr.drop(columns=['VIDEO_AVAILABLE'], inplace=True)
+
+        if not playoff_games_curr.empty:
+            playoff_games_curr = playoff_games_curr.sort_values(by=['GAME_ID']).reset_index(drop=True)
+            playoff_games_curr['LOCATION'] = np.where(playoff_games_curr['MATCHUP'].str.contains('vs.'), 'Home', 'Away')
+
+            conditions = [
+                playoff_games_curr['GAME_ID'].str.rfind('1', 7, 8) == 7,
+                playoff_games_curr['GAME_ID'].str.rfind('2', 7, 8) == 7,
+                playoff_games_curr['GAME_ID'].str.rfind('3', 7, 8) == 7,
+                playoff_games_curr['GAME_ID'].str.rfind('4', 7, 8) == 7
+            ]
+
+            choices = [
+                'First Round',
+                'Second Round',
+                'Conference Finals',
+                'Finals'
+            ]
+
+            playoff_games_curr['GAME_TYPE'] = np.select(conditions, choices, default='NOT FOUND')
+
+            # Duplicate game_ids are the stat lines of the two teams, so combine into one game
+            for i in range(0, len(playoff_games_curr.index), 2):
+                self.add_games(pd.concat([playoff_games_curr.iloc[[i]], playoff_games_curr.iloc[[i+1]]]).reset_index(drop=True))
+
+    def get_team_schedule(self, team) -> pd.DataFrame:
+        return self.games[self.games.MATCHUP.str.contains(team)]
 
 class TeamSchedule(Schedule):
     def __init__(self) -> None:
@@ -98,6 +144,9 @@ class TeamSchedule(Schedule):
 
     def add_games(self, games) -> None:
         return super().add_games(games)
+
+    def get_game(self, game_num) -> pd.DataFrame:
+        return self.games[self.games.index == game_num]
 
 class PR():
     def __init__(self) -> None:
@@ -121,24 +170,15 @@ class Season():
 
         self.teams = teams
     
-    def create_schedule(self):
-        regular_season_games_curr = get_games(self.year, 'Regular Season')
-        regular_season_games_curr.drop(columns=['VIDEO_AVAILABLE'], inplace=True)
-        regular_season_games_curr = regular_season_games_curr.sort_values(by=['GAME_ID']).reset_index(drop=True)
+    def initialise_schedule(self):
+        self.schedule.initialise(self.year)
 
-        for i in range(0, len(regular_season_games_curr.index), 2):
-            self.schedule.add_games(Game(pd.concat([regular_season_games_curr.iloc[[i]], regular_season_games_curr.iloc[[i+1]]]).reset_index(drop=True)))
-
-        self.schedule.add_games(regular_season_games_curr)
-
-        playoff_games_curr = get_games(self.year, 'Playoffs')
-        playoff_games_curr.drop(columns=['VIDEO_AVAILABLE'], inplace=True)
-
-        if not playoff_games_curr.empty:
-            playoff_games_curr = playoff_games_curr.sort_values(by=['GAME_ID']).reset_index(drop=True)
-            self.schedule.add_games(playoff_games_curr)
+    def get_team(self, team_name):
+        for team in self.teams:
+            if team.name == team_name:
+                return team
         
-        print(self.schedule)
+        raise Exception('get_team(' + team_name + '): Team not found')
 
 class Team():
     def __init__(self, name) -> None:
@@ -152,11 +192,18 @@ class Team():
     def add_to_schedule(self, games):
         self.schedule.add_games(games)
 
-    def create_schedule(self, seasons: List[Season]):
+    def initialise_schedule(self, seasons: List[Season]):
         for season in seasons:
-            season_games = season.schedule.games
-            season_games = season_games[season_games['TEAM_ABBREVIATION'] == self.name].reset_index(drop=True)
+            season_games = season.schedule.get_team_schedule(self.name)
+            season_games = season_games.reset_index(drop=True)
+            season_games['GAME_NUM'] = pd.factorize(season_games['GAME_ID'])[0] + 1
+            season_games = season_games.set_index('GAME_NUM')
             self.schedule.add_games(season_games)
+
+            # print('---------------------------------')
+            # print(self.name + ' Schedule')
+            # print('---------------------------------')
+            # print(season_games)
 
         # print(self.schedule)
 
@@ -191,10 +238,15 @@ class Team():
 #         print(playoff_game['SERIES_ID'])
 
 
+curr_time = time.time()
 
 season = Season(2021, create_team_names())
 
-season.create_schedule()
+season.initialise_schedule()
+print(season.schedule.games)
 
-for team in season.teams:
-    team.create_schedule(season.seasons)
+# for team in season.teams:
+#     team.initialise_schedule(season.seasons)
+
+# team: Team = season.get_team('GSW')
+# print(team.schedule.get_game(105))
