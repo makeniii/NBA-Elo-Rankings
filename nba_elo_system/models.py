a6d4bb063e1c2ee8c8a53c65f9a904212fa407f3
makeniii <makenifililava@gmail.com>
2022-12-20 15:02:53 +1100

    Updated models to reflect changes in schema

--- nba_elo_system/models.py
+++ nba_elo_system/models.py
@@ -2,43 +2,43 @@ from nba_elo_system import db
 
 
 class Season(db.Model):
-    Year = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String(7), unique=True, nullable=False)
-    Games = db.relationship('Game', backref='Season', lazy=True)
+    year = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(7), unique=True, nullable=False)
+    games = db.relationship('Game', backref='season', lazy=True)
 
     def __repr__(self):
-        return f"Season('{self.Year}', '{self.Name}')"
+        return f"Season('{self.year}', '{self.name}')"
 
 
 class Game(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    SeasonID = db.Column(db.Integer, db.ForeignKey('Season.Year'), nullable=False)
-    Type = db.Column(db.String(15), nullable=False)
-    Status = db.Column(db.Integer, nullable=False)
-    PlaysIns = db.relationship('PlaysIn', backref='Game', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    season_id = db.Column(db.Integer, db.ForeignKey('season.year'), nullable=False)
+    type = db.Column(db.String(15), nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+    plays_in = db.relationship('PlaysIn', backref='game', lazy=True)
 
     def __repr__(self) -> str:
-        return f"Game('{self.ID}', '{self.SeasonID}', '{self.Type}', '{self.Status}')"
+        return f"Game('{self.id}', '{self.season_id}', '{self.type}', '{self.status}')"
 
 
 class PlaysIn(db.Model):
-    GameID = db.Column(db.Integer, db.ForeignKey('Game.ID'), primary_key=True)
-    TeamID = db.Column(db.Integer, db.ForeignKey('Team.ID'), primary_key=True)
-    Score = db.Column(db.Integer, nullable=False)
-    Location = db.Column(db.String(4), nullable=False)
-    Outcome = db.Column(db.String(1), nullable=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(4), nullable=False)
+    outcome = db.Column(db.String(1), nullable=True)
 
     def __repr__(self) -> str:
-        return f"PlaysIn('{self.GameID}', '{self.TeamID}', '{self.Score}', '{self.Location}', '{self.Outcome}')"
+        return f"PlaysIn('{self.game_id}', '{self.team_id}', '{self.score}', '{self.location}', '{self.outcome}')"
 
 
 class Team(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.Text, nullable=False)
-    ShortName = db.Column(db.Text, nullable=False)
-    Abbreviation = db.Column(db.Text, nullable=False)
-    Elo = db.Column(db.Integer, nullable=False, )
-    PlaysIns = db.relationship('PlaysIn', backref='Team', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    short_name = db.Column(db.Text, nullable=False)
+    abbreviation = db.Column(db.Text, nullable=False)
+    elo = db.Column(db.Integer, nullable=False)
+    plays_in = db.relationship('PlaysIn', backref='team', lazy=True)
 
     def __repr__(self):
-        return f"Team('{self.ID}', '{self.Name}', '{self.ShortName}', '{self.Abbreviation}', '{self.Elo}')"
+        return f"Team('{self.id}', '{self.name}', '{self.short_name}', '{self.abbreviation}', '{self.elo}')"
