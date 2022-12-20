5b23b18598397f6047f6458984e8f0dc942681d4
makeniii <makenifililava@gmail.com>
2022-12-20 17:08:06 +1100

    Update to only calculate 22' season

--- nba_elo_system/routes.py
+++ nba_elo_system/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template, url_for
 from nba_elo_system import app
-from nba_elo_system.models import season_22
+from nba_elo_system.models import season_21
 
 
 @app.route('/')
 def index():
-    ordered_list = season_22.teams
+    ordered_list = season_21.teams
     ordered_list.sort(key=lambda x: x.elo.elo, reverse=True)
     return render_template('index.html', teams=ordered_list)
