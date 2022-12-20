ab520e28f0c093b4f8df14f4393408a19d1364cb
makeniii <makenifililava@gmail.com>
2022-12-20 15:01:19 +1100

    Fixed db URI

--- nba_elo_system/__init__.py
+++ nba_elo_system/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '691ca0a5f74f0ef59a7054473c6861a333b4df381ee0b774db053d598ca4075b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nba.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'nba.db')
 db = SQLAlchemy(app)
 
 from nba_elo_system import routes
