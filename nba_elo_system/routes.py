385c3e1f1dddcdc8d28fdfc3e22828d8e51b239e
makeniii <makenifililava@gmail.com>
2022-12-20 15:03:31 +1100

    Now using db to get data

--- nba_elo_system/routes.py
+++ nba_elo_system/routes.py
@@ -1,10 +1,9 @@
 from flask import render_template, url_for
-from nba_elo_system import app
-from nba_elo_system.models import season_22
+from nba_elo_system import app, db
+from nba_elo_system.models import Season, Game, PlaysIn, Team
 
 
 @app.route('/')
 def index():
-    ordered_list = season_22.teams
-    ordered_list.sort(key=lambda x: x.elo.elo, reverse=True)
-    return render_template('index.html', teams=ordered_list)
+    teams = Team.query.order_by(Team.elo.desc()).all()
+    return render_template('index.html', teams=teams)
