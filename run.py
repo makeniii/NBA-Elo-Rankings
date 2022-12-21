be133109326822a9e7ad5fe4ad63a1202a15cf0e
makeniii <makenifililava@gmail.com>
2022-12-21 19:17:59 +1100

    A check if db exists and also update db on start

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from nba_elo_system import app
+from nba_elo_system.db import update_db
+from os import path
+from pathlib import Path
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    dbpath = Path(__file__).parent / 'nba_elo_system/nba.db'
+    if path.isfile(dbpath):
+        update_db(dbpath)
+        app.run(debug=True)
+    else:
+        print('Please initialise DB before running server...', 'To initialise DB: run "nba_elo_system/db.py"')
