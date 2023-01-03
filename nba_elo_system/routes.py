21a15b31376c26fbad45dca47a354d20df3afe0b
makeniii <makenifililava@gmail.com>
2023-01-03 23:30:57 +1100

    Gets upcoming schedule and returns to template

--- nba_elo_system/routes.py
+++ nba_elo_system/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for
 from nba_elo_system import app, db
 from nba_elo_system.models import Season, Game, PlaysIn, Team
+import datetime
 
 
 @app.route('/')
@@ -34,4 +35,16 @@ def index():
 
 @app.route('/schedule')
 def schedule():
-    return render_template('schedule.html')
+    # get next day games only after 6pm AEST
+    start_date = (datetime.datetime.today() + datetime.timedelta(hours=6)).date()
+    dates = [start_date] + [start_date + datetime.timedelta(days=i) for i in range(1, 7)]
+    upcoming_week_schedule = PlaysIn.query.filter(PlaysIn.game_date.in_(dates)).order_by(PlaysIn.game_date.asc(), PlaysIn.game_id.asc(), PlaysIn.location.desc()).all()
+    upcoming_week_schedule = [{'home': {'playsin': upcoming_week_schedule[i]}, 'away': {'playsin': upcoming_week_schedule[i+1]}} for i in range(0, len(upcoming_week_schedule), 2)]
+
+    for game in upcoming_week_schedule:
+        game['home']['team'] = Team.query.filter(Team.id == game['home']['playsin'].team_id).one()
+        game['away']['team'] = Team.query.filter(Team.id == game['away']['playsin'].team_id).one()
+    
+    print(upcoming_week_schedule, len(upcoming_week_schedule))
+
+    return render_template('schedule.html', schedule=upcoming_week_schedule)
