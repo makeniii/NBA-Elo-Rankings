85721bf0c3eeb5a72557579219d14bfeeec118c5
makeniii <makenifililava@gmail.com>
2023-01-04 21:11:56 +1100

    Renders schedule.html with required data

--- nba_elo_system/routes.py
+++ nba_elo_system/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, url_for
 from nba_elo_system import app, db
 from nba_elo_system.models import Season, Game, PlaysIn, Team
+from nba_elo_system.elo_calculator import EloCalculator
 import datetime
+import pprint
 
 
 @app.route('/')
@@ -35,16 +37,84 @@ def index():
 
 @app.route('/schedule')
 def schedule():
-    # get next day games only after 6pm AEST
+    # get games from the following week
+    # only tick over to next day after 6pm AEST
     start_date = (datetime.datetime.today() + datetime.timedelta(hours=6)).date()
     dates = [start_date] + [start_date + datetime.timedelta(days=i) for i in range(1, 7)]
-    upcoming_week_schedule = PlaysIn.query.filter(PlaysIn.game_date.in_(dates)).order_by(PlaysIn.game_date.asc(), PlaysIn.game_id.asc(), PlaysIn.location.desc()).all()
-    upcoming_week_schedule = [{'home': {'playsin': upcoming_week_schedule[i]}, 'away': {'playsin': upcoming_week_schedule[i+1]}} for i in range(0, len(upcoming_week_schedule), 2)]
+    upcoming_week_schedule_data = PlaysIn.query.filter(PlaysIn.game_date.in_(dates)).order_by(PlaysIn.game_date.asc(), PlaysIn.game_id.asc(), PlaysIn.location.desc()).all()
+    upcoming_week_schedule_data = [{'home': {'playsin': upcoming_week_schedule_data[i]}, 'away': {'playsin': upcoming_week_schedule_data[i+1]}} for i in range(0, len(upcoming_week_schedule_data), 2)]
 
-    for game in upcoming_week_schedule:
+    mon_games = list()
+    tue_games = list()
+    wed_games = list()
+    thu_games = list()
+    fri_games = list()
+    sat_games = list()
+    sun_games = list()
+
+    for game in upcoming_week_schedule_data:
         game['home']['team'] = Team.query.filter(Team.id == game['home']['playsin'].team_id).one()
         game['away']['team'] = Team.query.filter(Team.id == game['away']['playsin'].team_id).one()
+        game_day = game['home']['playsin'].game_date.strftime('%A')
+        game['home'] = game['home']['team']
+        game['away'] = game['away']['team']
+        
+        if game_day == 'Monday':
+            mon_games.append(game)
+        elif game_day == 'Tuesday':
+            tue_games.append(game)
+        elif game_day == 'Wednesday':
+            wed_games.append(game)
+        elif game_day == 'Thursday':
+            thu_games.append(game)
+        elif game_day == 'Friday':
+            fri_games.append(game)
+        elif game_day == 'Saturday':
+            sat_games.append(game)
+        elif game_day == 'Sunday':
+            sun_games.append(game)
     
-    print(upcoming_week_schedule, len(upcoming_week_schedule))
+    week_schedule = [
+        {
+            'day': 'Monday',
+            'games': mon_games
+        },
+        {
+            'day': 'Tuesday',
+            'games': tue_games
+        },
+        {
+            'day': 'Wednesday',
+            'games': wed_games
+        },
+        {
+            'day': 'Thursday',
+            'games': thu_games
+        },
+        {
+            'day': 'Friday',
+            'games': fri_games
+        },
+        {
+            'day': 'Saturday',
+            'games': sat_games
+        },
+        {
+            'day': 'Sunday',
+            'games': sun_games
+        }
+    ]
 
-    return render_template('schedule.html', schedule=upcoming_week_schedule)
+    for i in range(7):
+        week_schedule[dates[i].weekday()]['date'] = dates[i]
+
+    week_schedule.sort(key=lambda x: x['date'])
+
+    return render_template(
+        'schedule.html', 
+        week_schedule=week_schedule, 
+        round=round, 
+        win_expectancy=EloCalculator.win_expectancy,
+        projected_point_diff=EloCalculator.projected_point_diff,
+        score_round=lambda x: x if x % 1 == 0.5 else round(x)
+        )
