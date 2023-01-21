aaf542024dd99d32c557ed0f6e7f3e46df61044c
makeniii <makenifililava@gmail.com>
2023-01-21 17:14:45 +1100

    Move db_path to init.py for global access

--- nba_elo_system/__init__.py
+++ nba_elo_system/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from pathlib import Path
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '691ca0a5f74f0ef59a7054473c6861a333b4df381ee0b774db053d598ca4075b'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'nba.db')
 db = SQLAlchemy(app)
+db_path = Path(__file__).parent / 'nba.db'
 
 from nba_elo_system import routes
--- run.py
+++ run.py
@@ -1,15 +1,11 @@
-from nba_elo_system import app
+from nba_elo_system import app, db_path
 from nba_elo_system.db import update_db, initialise_db
 from os import path
 from pathlib import Path
 
 if __name__ == '__main__':
-    dbpath = Path(__file__).parent / 'nba_elo_system/nba.db'
-    
-    if path.isfile(dbpath):
-        update_db(dbpath)
-    else:
+    if not path.isfile(db_path):
         print(' * Please wait while initialising the server...')
-        initialise_db(dbpath)
+        initialise_db(db_path)
     
     app.run(debug=True)
